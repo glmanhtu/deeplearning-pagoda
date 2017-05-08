71df473df8f5379b938c3b6a8bfa3130d496f81f
Vu Manh Tu <tuvm@athena.studio>
2017-05-08 07:38:16 +0700

    Update

--- utils/caffe.py
+++ utils/caffe.py
@@ -36,7 +36,7 @@ class Caffe(object):
         solver = os.path.abspath(solver)
         log = os.path.abspath(log)
         caffe_bin = self.caffe_home() + "/build/tools/caffe"
-        command = ["nohub", caffe_bin, "train", "--solver=" + solver, "--weights", trained_model_file, "2>&1 >", log, "&"]
+        command = ["/usr/bin/nohup", caffe_bin, "train", "--solver=" + solver, "--weights", trained_model_file, "2>&1 >", log, "&"]
         command = ' '.join(command)
         execute_train_command(command)
         execute("tail -f " + log)
